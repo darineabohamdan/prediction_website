71a7a7ecf65b00c762e90c41214ae8ade7a0e9b8
darineabohamdan <darine.abohamdan@gmail.com>
2022-02-06 12:02:27 +0200

    Added the code that gets me the predicted age for the name entered by the user.

--- solution.js
+++ solution.js
@@ -1,6 +1,13 @@
-
+const age = document.getElementById("age");
+const gender = document.getElementById("gender");
+const country = document.getElementById("country");
 const dog_image = document.getElementById("dog_image");
+const btn = document.querySelector('button');
+
+
 //On refresh, random dog picture appears.
+document.getElementById("name").value;
+
 window.onload = function getImage() {
 
     fetch('https://dog.ceo/api/breeds/image/random')
@@ -9,6 +16,45 @@ window.onload = function getImage() {
             dog_image.innerHTML = `<img src="${data.message}"/>`
         })
 }
+
+//Getting age.
+
+btn.addEventListener("click", async () => {
+
+    const input = document.querySelector("input").value;
+    const data = await fetchApi(input);
+    document.querySelector("#age").innerHTML = data.age;
+    
+
+    async function fetchApi(name) {
+        const url = "https://api.agify.io/?name=" + name;
+        const dataFetch = await fetch(url, {
+            method: "GET",
+            headers: {
+                accept: "application/json",
+            },
+        })
+        const data = await dataFetch.json();
+
+        return data;
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
